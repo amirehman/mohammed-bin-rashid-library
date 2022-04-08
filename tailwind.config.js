15cbc4d2bbfb8ecd106c8b36bfe088e4a9215453
Aamir <aamirnorth@gmail.com>
2022-04-08 10:51:57 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'theme-orange': '#e24e16',
+        'theme-dark-yellow': '#f1ab3d',
+        'theme-purple': '#522b46',
+        'theme-teal': '#a5c5b8',
+        'theme-light-gray': '#f2f2f2',
+        'theme-gray': '#9b9b9b',
+        'theme-black': '#000000',
+        'theme-light-belge': '#f6dfa4',
+      }
+    },
+  },
+  plugins: [],
+}
